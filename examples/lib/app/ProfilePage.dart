d0e2b6441bfba8a97d667f4de034f52cd007c5cc
pilsogood <good.seungpil.choi@gmail.com>
2019-01-01 12:25:14 +0900

    예제 연습

--- examples/lib/app/ProfilePage.dart
+++ examples/lib/app/ProfilePage.dart
@@ -15,23 +15,50 @@ Color gradientEnd = Color.fromRGBO(208, 78, 214 , 1); //Change end gradient colo
 
 class _AppStatus extends State<ProfilePage> {
   
+  double cWidth = 0.0;
+  double itemHeight = 28.0;
+  double itemsCount = 20;
+  double screenWidth;
+
   final userId = TextEditingController();
   final userPassword = TextEditingController();
 
-  @override 
+  final controller = ScrollController();
+
+  
+  @override
   void initState() {
     super.initState();
+    controller.addListener(onScroll);
+  }
+
+  onScroll() {
+    setState(() {
+     cWidth = controller.offset * screenWidth / (itemHeight * itemsCount);
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(onScroll);
   }
 
   @override
   Widget build(BuildContext context) {
+    
+    screenWidth = MediaQuery.of(context).size.width;
+
     return new Scaffold(
-      body:  new Column(
+      body:  
+      new Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           // new CustomAppBar('test'),              
-          _peoplesSample()
+          new Flexible(
+            // child: _peoples(controller),
+            child: _testMenu(controller,200.0)
+          ) 
         ],
       )
     );
@@ -192,8 +219,9 @@ class CustomAppBar extends StatelessWidget {
   }
 }
 
-Widget _peoples() {
+Widget _peoples(controller) {
   return new ListView.builder(
+    controller: controller,
     itemCount: 100,
     itemBuilder: (BuildContext context, i) {
       return new ListTile(
@@ -250,8 +278,8 @@ Widget _profile() {
 Widget _profileImage() {
   return new Center(
     child: new Container(
-      width: 100.0,
-      height: 100.0,
+      width: 80.0,
+      height: 80.0,
       decoration: new BoxDecoration(
         shape: BoxShape.circle,
         image: new DecorationImage(
@@ -267,5 +295,102 @@ Widget _profileImage() {
         ],
       )
     ),
+  );
+}
+
+Widget _testMenu(controller,appBarHeight )  {
+
+return SafeArea(
+    child: CustomScrollView(
+      controller: controller,
+      slivers: <Widget>[
+        
+        SliverAppBar(
+          pinned: true,
+          expandedHeight: appBarHeight,
+          floating: false,
+          flexibleSpace: Stack(
+            overflow: Overflow.clip, 
+            children: <Widget>[
+              // new CustomAppBar('test')
+              new Container(
+                decoration: new BoxDecoration(
+                    border: new Border.all(width: 1.0, color: Colors.transparent),
+                    // shape: BoxShape.circle,
+                    color: const Color(0xff7c94b6),
+                    image: new DecorationImage(
+                      colorFilter: new ColorFilter.mode(Colors.black38.withOpacity(0.5), BlendMode.srcATop),
+                      image: new AssetImage('assets/images/intro-bg-18.jpg'),
+                      fit: BoxFit.cover,
+                      alignment: Alignment(0.0, 1.0),
+                    ),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, -10.0),
+                        blurRadius: 20.0,
+                      ),
+                    ],
+                 ),
+                 child: 
+                  new Stack(
+                    children: <Widget>[
+                       Center(
+                          child: new ClipRRect(
+                            borderRadius: new BorderRadius.circular(100.0),
+                            clipBehavior: Clip.hardEdge,
+                            child: new Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                _profileImage(),
+                                new Padding(
+                                  padding: new EdgeInsets.fromLTRB(0.0, 8.0, 0.0, 8.0)
+                                ),
+                                // new SizedBox(
+                                //   width: 200.0,
+                                //   height: 300.0,
+                                // ),
+                                new Text('@Seungpil Choi', 
+                                style: new TextStyle(
+                                  fontStyle: FontStyle.italic, 
+                                  fontSize: 15.0,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold
+                                )
+                              )
+                              ],
+                            )
+                            
+                          ),
+                        ),
+                    ]
+                  )
+              ),
+            ],
+          ),
+        ),
+        _peoplesList(),
+        // SliverList(delegate:
+        //     SliverChildBuilderDelegate((BuildContext context, int index) {
+        //   return ListTile(title: Text("Flutter / $index"));
+        // }))
+      ],
+    ),
+  );
+}
+
+Widget _peoplesList() {
+  return new SliverList(
+    delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+      return new ListTile(
+        title: new Text('Name'),
+        subtitle: new Text('subname'),
+        leading: new CircleAvatar(
+          backgroundImage:
+            new NetworkImage("https://www.tripgrida.com/img/profile/1_1448624008.jpg"),
+          )
+       );
+     }
+    )
   );
 }
