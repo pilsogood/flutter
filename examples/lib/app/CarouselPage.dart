14ccdd3359d17c2eeb7480e3d43d13572f588716
pilsogood <good.seungpil.choi@gmail.com>
2019-01-02 14:58:24 +0900

    예제 실습

--- examples/lib/app/CarouselPage.dart
+++ examples/lib/app/CarouselPage.dart
@@ -35,6 +35,11 @@ class CarouselPage extends StatefulWidget {
 //   _MyHomePageState createState() => new _MyHomePageState();
 // }
 
+
+Color gradientStart = Color.fromRGBO(131, 77, 155 , 1); //Change start gradient color here
+Color gradientMiddle = Color.fromRGBO(207, 139, 243 , 1); //Change end gradient color here
+Color gradientEnd = Color.fromRGBO(208, 78, 214 , 1); //Change end gradient color here
+
 class _AppStatus extends State<CarouselPage> {
 
   @override
@@ -42,7 +47,7 @@ class _AppStatus extends State<CarouselPage> {
     return new Scaffold(
       appBar: new AppBar(
         // title: new Text(widget.title),
-         title: new Text('test'),
+         title: new Text('test1'),
       ),
     body: 
     // new Swiper(
@@ -54,21 +59,71 @@ class _AppStatus extends State<CarouselPage> {
     //     control: new SwiperControl(),
     //   ),
     new Container(
-      height: 175.0,
+       
+      //  decoration: new BoxDecoration(
+      //       color: const Color(0xff7c94b6),
+      //       gradient: new LinearGradient(colors: [gradientStart, gradientEnd],
+      //           begin: const FractionalOffset(0.5, 0.0),
+      //           end: const FractionalOffset(0.0, 0.5),
+      //           stops: [0.0,1.0],
+      //           tileMode: TileMode.clamp
+      //       ),
+      //   ),
+
+
+      // padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
       // width: 175.0,
-          child:
-          new Swiper(
+
+          child: new Container(
+            height: 300.0,
+            margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 20.0),
+            child: new Swiper(
                 itemBuilder: (BuildContext context, int index) {
-                  return new Image.network(
-                    "http://via.placeholder.com/288x188",
-                    fit: BoxFit.fill,
+                  // return new Image.asset(
+                  //   "assets/images/intro-bg-5.jpg",
+                  //   fit: BoxFit.fill,
+                  // );
+
+                    return  new Container(
+                      margin: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 50.0),
+                      child: new DecoratedBox(
+                        decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: new BorderRadius.circular(20.0),
+                        boxShadow: <BoxShadow>[
+                          new BoxShadow(
+                            color: new Color(0xFFF8E1FE),
+                            blurRadius: 10.0,
+                            offset: new Offset(0.0, 5.0),
+                          ),
+                        ],
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/intro-bg-5.jpg"),
+                          fit: BoxFit.fill,
+                        )
+                      ),
+                   ),
                   );
                 },
                 itemCount: 10,
-                viewportFraction: 0.8,
-                scale: 0.9,
+                // viewportFraction: 0.8,
+                // scale: 0.9,
+                itemWidth: 300.0,
+                pagination: new SwiperPagination(
+                  margin: EdgeInsets.all(10),
+                  
+                  ),
+                control: new SwiperControl(
+                ),
+                layout: SwiperLayout.STACK,
+
+                // itemCount: 10,
+                // itemWidth: 300.0,
+                // itemHeight: 400.0,
+                // layout: SwiperLayout.TINDER,
               ),
       ),
+    ),
     );
   }
 }
