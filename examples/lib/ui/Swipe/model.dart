6051008d99cc109c5994e24e85764092eae75bad
pilsogood <good.seungpil.choi@gmail.com>
2019-01-10 06:55:17 +0900

    예제 실습

--- examples/lib/ui/Swipe/model.dart
+++ examples/lib/ui/Swipe/model.dart
@@ -19,12 +19,16 @@ final List<Profile> demoProfiles = [
     photos: [
       'assets/images/profile/profile-5.jpg',
       'assets/images/profile/profile-6.jpg',
+      'assets/images/profile/profile-5.jpg',
+      'assets/images/profile/profile-6.jpg',
     ],
     name: 'super dog',
     bio: 'You better swipe left...',
   ),
   new Profile(
     photos: [
+      'assets/images/profile/profile-1.jpg',
+      'assets/images/profile/profile-2.jpg',
       'assets/images/profile/profile-1.jpg',
       'assets/images/profile/profile-2.jpg',
     ],
@@ -35,12 +39,16 @@ final List<Profile> demoProfiles = [
     photos: [
       'assets/images/profile/profile-3.jpg',
       'assets/images/profile/profile-4.jpg',
+      'assets/images/profile/profile-3.jpg',
+      'assets/images/profile/profile-4.jpg',
     ],
     name: 'super dog',
     bio: 'You better swipe left...',
   ),
   new Profile(
     photos: [
+      'assets/images/profile/profile-5.jpg',
+      'assets/images/profile/profile-6.jpg',
       'assets/images/profile/profile-5.jpg',
       'assets/images/profile/profile-6.jpg',
     ],
